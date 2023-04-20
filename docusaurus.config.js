36b001146556451be919f30b33eaba7daf613b68
Onat <53895969+onattech@users.noreply.github.com>
2023-04-20 14:40:22 +0300

    🛠️ Updated configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,11 +73,11 @@ const config = {
             // Replace with your project's social card
             image: "img/docusaurus-social-card.jpg",
             navbar: {
-                title: "My Site",
-                logo: {
-                    alt: "My Site Logo",
-                    src: "img/logo.svg",
-                },
+                title: "Portfolio",
+                // logo: {
+                //     alt: "My Site Logo",
+                //     src: "img/logo.svg",
+                // },
                 items: [
                     {
                         type: "docSidebar",
