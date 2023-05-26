f15e65ada2803f9d0a83c4d8f5d39bfa117d53d3
onattech <53895969+onattech@users.noreply.github.com>
2023-05-26 18:10:50 +0000

    deploy: 1d8362f477b3685e23918d93dbf8e773caf2f5cc

---
+++ assets/js/1df93b7f.ee7126cc.js
@@ -0,0 +1,337 @@
+"use strict";(self.webpackChunkdev_journal=self.webpackChunkdev_journal||[]).push([[237],{9913:(e,t,a)=>{a.r(t),a.d(t,{GlobalStyle:()=>m,default:()=>d});var n=a(9496),i=a(3089);const r=a.p+"assets/images/profile-5bc73eac6f8211920b1ff8bf5a029047.jpg",l=a.p+"assets/images/dataplane-c22c1910ba039744ea19f0086b455781.png";var o=a(9004);const m=o.vJ`
+  :root {
+    scroll-behavior: smooth;
+  }
+
+  h1,h2,p {
+    font-family: Montserrat, sans-serif;
+  }
+
+  body {
+    text-align: center;
+  }
+
+  a,
+  a:active,
+  a:hover,
+  a:link,
+  a:visited {
+    text-decoration: none;
+  }
+`;function d(){n.useLayoutEffect((()=>{document.querySelector("div.navbar__items:nth-child(2) > div").style.display="none"}),[]);const[e,t]=n.useState(null);return n.useEffect((()=>{let e=!0;return(async()=>{const a=await fetch("https://api.github.com/repos/dataplane-app/dataplane");await new Promise((e=>setTimeout(e,1e3)));const n=await a.json();e&&t(n.watchers_count)})().catch(console.error),()=>{e=!1}}),[]),n.createElement(i.Z,{title:"Onat",description:"My portfoio and dev insights"},n.createElement(m,null),n.createElement("main",null,n.createElement(p,null,n.createElement(s,null,n.createElement(g,null,"Hi, my name is ",n.createElement(c,null,"Onat"),n.createElement("br",null),"I'm a full-stack Developer."),n.createElement(h,null,n.createElement(u,{rel:"noreferrer",href:"#about"},"Know more")))),n.createElement("div",{id:"about",style:{marginTop:"-60px",position:"absolute"}}),n.createElement(b,null,n.createElement(s,null,n.createElement(x,null,"About me"),n.createElement(w,null,n.createElement(k,null,n.createElement(y,null,n.createElement(E,{alt:"Profile Image",height:"auto",width:"300px",src:r}))),n.createElement(k,null,n.createElement(v,null,n.createElement(P,null,"I am Onat, a US citizen. I speak English and Turkish fluently and Arabic at an acceptable level. I am a full stack Web Developer with network engineering background. The stack that I am most comfortable with is React in the frontend and Go in the backend."),n.createElement(P,null,"Currently focusing on improving my skills on Go and learning docker and kubernetes."),n.createElement(Z,null,n.createElement(z,{rel:"noreferrer",target:"_blank",href:"/resume.pdf"},"View Resume"," "),n.createElement(z,{rel:"noreferrer",target:"_blank",style:{marginLeft:"20px"},href:"https://github.com/onattech"},"Github Profile"))))))),n.createElement(_,null,n.createElement(s,null,n.createElement("div",{style:{marginBottom:"9.375rem"}},n.createElement(x,{style:{color:"#272341"}},"Projects"),n.createElement(w,null,n.createElement(S,null,n.createElement(j,null,n.createElement(G,null,"Dataplane (",e," Github \ud83c\udf1f\ufe0fs)"),n.createElement("div",null,n.createElement("p",{style:{textAlign:"left",marginBottom:"0.9375rem",lineHeight:"1.5"}},"I have helped build and currently maintaining Dataplane as a full-stack engineer. Dataplane is an open source Airflow inspired data platform to automate, schedule and design data pipelines and workflows written in Go. I am responsible with all things frontend and setting up GraphQL endpoints, writing tests and more in the backend with Go. Check out my"," ",n.createElement("a",{rel:"noreferrer",href:"https://github.com/dataplane-app/dataplane/graphs/contributors",target:"_blank"},"contributions."))),n.createElement(T,{rel:"noreferrer",target:"_blank",href:"https://dataplane.app/"},"See Live"),n.createElement(C,{rel:"noreferrer",target:"_blank",href:"https://github.com/dataplane-app/dataplane"},"Source Code"))),n.createElement(I,null,n.createElement("div",{style:{margin:"0 auto"}},n.createElement("div",null,n.createElement("img",{alt:"Project Image",src:l})))))))),n.createElement(M,null,n.createElement(s,{style:{maxWidth:"480px"}},n.createElement(w,null,n.createElement(x,null,"Contact"),n.createElement("form",{action:"https://formspree.io/xqkyrbrr",method:"POST",style:{textAlign:"left",fontSize:"1.3rem"}},n.createElement("label",{htmlFor:"name",style:{display:"inline-block",width:"80px"}},"Name"),n.createElement("input",{type:"text",name:"name",id:"name"}),n.createElement("br",null),n.createElement("label",{htmlFor:"_replyto",style:{marginTop:"10px",display:"inline-block",width:"80px"}},"Email"),n.createElement("input",{type:"email",name:"_replyto",id:"email",style:{marginBottom:"20px"}}),n.createElement("br",null),n.createElement("label",{htmlFor:"textarea",id:"textarealabel"},"How may I help you?"),n.createElement("textarea",{name:"body",style:{width:"100%"},rows:8,cols:50}),n.createElement("br",null),n.createElement("button",{value:"Submit",style:{border:"none",borderRadius:"3px",fontSize:"1rem",marginTop:"10px",padding:"10px",fontFamily:"Montserrat,sans-serif"}},"Submit")))))))}const c=o.ZP.span`
+    background-image: linear-gradient(135deg, #02aab0 0%, #00cdac 100%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    color: #0000;
+`,p=o.ZP.section`
+    align-items: center;
+    background: #fff;
+    border-bottom: 0;
+    color: #272341;
+    display: flex;
+    font-weight: 400;
+    height: 100vh;
+    margin-bottom: 0;
+    min-height: 100vh;
+    padding: 0 3.5rem;
+    margin-top: -60px;
+
+    @media (max-width: 576px) {
+        padding: 0 1rem;
+    }
+`,s=o.ZP.div`
+    width: 100%;
+    padding-right: 0.46875rem;
+    padding-left: 0.46875rem;
+    margin-left: auto;
+    margin-right: auto;
+
+    @media (min-width: 576px) {
+        max-width: 540px;
+    }
+
+    @media (min-width: 768px) {
+        max-width: 720px;
+    }
+
+    @media (min-width: 992px) {
+        max-width: 960px;
+    }
+
+    @media (min-width: 1200px) {
+        max-width: 1140px;
+    }
+
+    @media (min-width: 1400px) {
+        max-width: 1320px;
+    }
+`,g=o.ZP.h1`
+    visibility: visible;
+
+    font-size: 2.1875rem;
+    font-weight: 700;
+    margin-bottom: 2rem;
+    text-align: left;
+    line-height: 1.2;
+    margin-top: 0;
+
+    @media (min-width: 576px) {
+        font-size: 2.1875rem;
+    }
+
+    @media (min-width: 768px) {
+        font-size: 2.25rem;
+    }
+
+    @media (min-width: 992px) {
+        font-size: 2.25rem;
+    }
+
+    @media (max-width: 992px) {
+        text-align: center;
+    }
+
+    @media (min-width: 1200px) {
+        font-size: 3.5rem;
+    }
+
+    @media (min-width: 1400px) {
+        font-size: 3.5rem;
+    }
+`,h=o.ZP.p`
+    display: flex;
+
+    @media (max-width: 992px) {
+        justify-content: center;
+    }
+`,f=o.ZP.a`
+    font-family: Montserrat, sans-serif;
+
+    display: inline-block;
+    font-weight: 700;
+    line-height: 1;
+    padding: 0.5rem 1rem;
+    position: relative;
+    transition: all 0.6s cubic-bezier(0.19, 1, 0.22, 1);
+    z-index: 1;
+    &: after {
+        bottom: 0;
+        content: "";
+        display: block;
+        height: 100%;
+        left: 0;
+        position: absolute;
+        transition: all 0.3s cubic-bezier(0.19, 1, 0.22, 1);
+        width: 0;
+        z-index: -1;
+    }
+`,u=(0,o.ZP)(f)`
+    font-size: 1.5rem;
+    @media (max-width: 600px) {
+        font-size: 1.25rem;
+    }
+
+    border: 2px solid transparent;
+    border-image: linear-gradient(135deg, #02aab0, #00cdac);
+    border-image-slice: 1;
+
+    -webkit-text-fill-color: transparent;
+    -webkit-background-clip: text;
+    background-image: linear-gradient(135deg, #02aab0, #00cdac);
+    color: transparent;
+
+    &:after {
+        background-image: linear-gradient(135deg, #02aab0, #00cdac);
+    }
+
+    &:hover {
+        -webkit-text-fill-color: #fff;
+        transition: all 0.3s ease-in-out;
+
+        &:after {
+            width: 100%;
+        }
+    }
+`,b=o.ZP.section`
+    padding: 3.125rem 0;
+
+    background-color: #02aab0;
+    background-image: linear-gradient(135deg, #02aab0, #00cdac);
+    border-top: 0;
+    -webkit-clip-path: polygon(0 0, 100% 0, 100% 80%, 0 100%);
+    clip-path: polygon(0 0, 100% 0, 100% 80%, 0 100%);
+    color: #fff;
+    height: 100%;
+    padding-bottom: 10%;
+
+    @media (max-width: 1200px) {
+        clip-path: none;
+        height: 100%;
+    }
+`,x=o.ZP.h2`
+    font-size: 2.5rem;
+    font-weight: 700;
+    margin: 0 0 2.8125rem;
+    text-transform: uppercase;
+
+    @media (max-width: 600px) {
+        font-size: 1.75rem;
+    }
+`,w=o.ZP.div`
+    --bs-gutter-x: 0.9375rem;
+    --bs-gutter-y: 0;
+    display: flex;
+    flex-wrap: wrap;
+    margin-left: calc(var(--bs-gutter-x) * -0.5);
+    margin-right: calc(var(--bs-gutter-x) * -0.5);
+    margin-top: calc(var(--bs-gutter-y) * -1);
+
+    & > * {
+        flex-shrink: 0;
+        margin-top: var(--bs-gutter-y);
+        max-width: 100%;
+        padding-left: calc(var(--bs-gutter-x) * 0.5);
+        padding-right: calc(var(--bs-gutter-x) * 0.5);
+        width: 100%;
+    }
+`,k=o.ZP.div`
+    @media (min-width: 576px) {
+        flex: 0 0 auto;
+        width: 100%;
+    }
+
+    @media (min-width: 768px) {
+        flex: 0 0 auto;
+        width: 50%;
+    }
+`,y=o.ZP.div`
+    align-items: center;
+    display: flex;
+    height: 100%;
+    justify-content: center;
+
+    @media (max-width: 768px) {
+        padding-bottom: 4rem;
+    }
+`,E=o.ZP.img`
+    border-radius: 0.15625rem !important;
+    box-shadow: 0 0.625rem 1.875rem rgba(0, 0, 0, 0.175) !important;
+    height: auto;
+    max-width: 100%;
+`,v=o.ZP.div`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    justify-content: center;
+`,P=o.ZP.p`
+    text-align: left;
+    margin-bottom: 0.625rem;
+    margin-top: 0;
+
+    @media (max-width: 768px) {
+        text-align: center;
+    }
+`,Z=o.ZP.span`
+    margin-top: 1rem !important;
+
+    display: flex !important;
+`,z=(0,o.ZP)(f)`
+    border: 2px solid #fff;
+    color: #fff;
+
+    &:after {
+        background: #fff;
+    }
+
+    &:hover::after {
+        width: 100%;
+    }
+`,_=o.ZP.section`
+    padding: 3.125rem 0;
+
+    background-color: #fff;
+    color: #272341;
+    margin-top: -6.25rem;
+    padding-top: 9.375rem;
+
+    @media (max-width: 576px) {
+        border: none;
+        padding-left: 0.625rem;
+        padding-right: 0.625rem;
+    }
+
+    @media (max-width: 1200px) {
+        margin-top: 0;
+        padding-top: 3.125rem;
+    }
+`,S=o.ZP.div`
+    @media (min-width: 576px) {
+        flex: 0 0 auto;
+        width: 100%;
+    }
+
+    @media (min-width: 992px) {
+        flex: 0 0 auto;
+        width: 33.3%;
+    }
+`,I=o.ZP.div`
+    @media (min-width: 576px) {
+        flex: 0 0 auto;
+        width: 100%;
+    }
+
+    @media (min-width: 992px) {
+        flex: 0 0 auto;
+        width: 66.6%;
+    }
+`,j=o.ZP.div`
+    text-align: left;
+
+    @media (max-width: 1200px) {
+        margin-bottom: 3rem;
+    }
+`,G=o.ZP.h3`
+    font-size: 1.5625rem;
+    font-weight: 700;
+    margin-bottom: 1.125rem;
+    font-family: Montserrat, sans-serif;
+`,T=(0,o.ZP)(z)`
+    -webkit-text-fill-color: transparent;
+    -webkit-background-clip: text;
+    background-image: linear-gradient(135deg, #02aab0, #00cdac);
+    color: transparent;
+
+    border: 2px solid transparent;
+    border-image: linear-gradient(135deg, #02aab0, #00cdac);
+    border-image-slice: 1;
+
+    &:after {
+        background-image: linear-gradient(135deg, #02aab0, #00cdac);
+    }
+
+    &:hover {
+        -webkit-text-fill-color: #fff;
+    }
+`,C=(0,o.ZP)(f)`
+    -webkit-text-fill-color: transparent;
+    -webkit-background-clip: text;
+    background-image: linear-gradient(135deg, #02aab0, #00cdac);
+    color: transparent;
+`,M=o.ZP.section`
+    background-image: linear-gradient(135deg, #02aab0, #00cdac);
+    -webkit-clip-path: polygon(0 15vh, 100% 0, 100% 100%, 0 100%);
+    clip-path: polygon(0 15vh, 100% 0, 100% 100%, 0 100%);
+    color: #fff;
+    margin-bottom: -1px;
+    margin-top: -6.25rem;
+    padding: 9.375rem 0 6.25rem;
+
+    font-family: Montserrat, sans-serif;
+
+    @media (max-width: 1200px) {
+        -webkit-clip-path: none;
+        margin-top: 0;
+        padding: 6.25rem 0;
+    }
+`}}]);
